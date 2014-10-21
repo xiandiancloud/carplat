d837635c0aac3ef803814adbb1f91cea028861f9
wang1030 <1010982758@qq.com>
2014-10-21 16:40:24 +0800

    1

--- src/com/dhl/web/UploadController.java
+++ src/com/dhl/web/UploadController.java
@@ -191,7 +191,7 @@ public class UploadController {
 //			 List<String> dataList=new ArrayList<String>();
 		        
 			 String path = request.getSession().getServletContext().getRealPath("/export/");
-			 File file = new File(path+"/人员信息.csv");
+			 File file = new File(path+"/常用车辆信息.csv");
 			 if (!file.exists())
 			 {
 				 file.createNewFile();
@@ -281,7 +281,7 @@ public class UploadController {
 //			 List<String> dataList=new ArrayList<String>();
 		        
 			 String path = request.getSession().getServletContext().getRealPath("/export/");
-			 File file = new File(path+"/临时人员信息.csv");
+			 File file = new File(path+"/访客车辆信息.csv");
 			 if (!file.exists())
 			 {
 				 file.createNewFile();
